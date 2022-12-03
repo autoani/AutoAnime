805cbbd7f9b24e830713fddce779de2d9cc35de1
autoani <119661112+autoani@users.noreply.github.com>
2022-12-04 02:13:32 +0630

    Update config.py

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://autoani:autoani@autoani.enm
 INDEX_ID = int(getenv("INDEX_ID", -1001858537966))
 UPLOADS_ID = int(getenv("UPLOADS_ID", -1001521731149))
 
-STATUS_ID = int(getenv("STATUS_ID", 2))
-SCHEDULE_ID = int(getenv("SCHEDULE_ID", 3))
+STATUS_ID = int(getenv("STATUS_ID", 6))
+SCHEDULE_ID = int(getenv("SCHEDULE_ID", 7))
 
 CHANNEL_TITLE = getenv("CHANNEL_TITLE", "UTUAnimePahe")
 INDEX_USERNAME = getenv("INDEX_USERNAME", "UTUindex")
